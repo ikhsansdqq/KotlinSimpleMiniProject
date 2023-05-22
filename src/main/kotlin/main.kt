e89f4b81beec3712e4b8381393023fda2c555f60
Ikhsan Assidiqie <91542488+ikhsansdqq@users.noreply.github.com>
2023-05-22 16:04:26 +0700

    Commit 22/05/23 - Coffee Shop System

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,31 +1,56 @@
+data class MenuItem(val name: String, val price: Float)
+//data class CustomersName(val name: String, val deposit: Float)
+
 fun main() {
-//   COFFEE SHOP CUSTOMER SUPPORT PROGRAM
+//    val customersData = arrayOf(
+//        CustomersName("John McTavish", 16F),
+//        CustomersName("John Price", 22F),
+//        CustomersName("Philip Graves", 40F),
+//        CustomersName("Alejandro Vargas", 37F)
+//    )
+
+    val beveragesMenu = arrayOf(
+        MenuItem("Latte", 4.16F),
+        MenuItem("Americano", 2.25F),
+        MenuItem("Coffee Chocolate", 2.39F),
+        MenuItem("Piccolo", 2.70F)
+    )
+
     print("Hi! May I have your name? ")
+
     val customerName = readln()
     println("What would you like to order for today Mr. $customerName?")
-    println("We have \n1. Latte ($4.16)\n2. Ice Coffee ($2.25)\n3. Chocolate ($2.39)")
+
+    for (i in beveragesMenu.indices) {
+        println("${i + 1}. ${beveragesMenu[i].name} ($${beveragesMenu[i].price})")
+    }
+
     print("May I inquire about your preferred choice of beverages? ")
-    val menuPick = readln().toInt()
-    when (menuPick) {
-        1 -> {
-            println("Thank you! Your Latte will be prepared within a few minutes. We appreciate your choice and hope you enjoy your drink!")
-        }
 
-        2 -> {
+    when (val choice = readln().toInt()) {
+        in 1..beveragesMenu.size -> {
+            val selectedBeverage = beveragesMenu[choice - 1]
+            print("How many ${selectedBeverage.name} do you want? ")
+            val howMany = readln().toInt()
+            println("\n=== ORDERS FOR $howMany ${selectedBeverage.name.uppercase()} ===")
+            val pricePerItem = selectedBeverage.price
 
-        }
+            println("Do you want to add size up?")
 
-        3 -> {
-
+            val subTotal = howMany * pricePerItem
+            val discountThreshold = 10F
+            val discountRate = 0.1F
+            val total = if (subTotal > discountThreshold) {
+                println("Woohoo! Congrats on scoring a 10% discount for your beverages! It'll be automatically applied to your bill. Enjoy! \uD83C\uDF89")
+                subTotal * (1 - discountRate)
+            } else {
+                subTotal
+            }
+            println("The total bill amount is: $${"%.2f".format(total)}")
         }
 
         else -> {
             println("It seems that you haven't chosen any beverages. That's perfectly alright. I hope you have a pleasant day! :)")
         }
     }
-
-//    NANTI MASUKIN SALDO BUYERS
-//    TERUS KURANGIN SALDO SAMA HARGA YANG DIBELI
-//    KALO BISA DIJADIIN ARRAY MINUMAN SAMA HARGANYA
-//    ATAU TAMBAHIN FITUR ADD MORE ORDERS TERUS BIKIN FUNGSI BUAT NGETOTALIN HARGA
 }
