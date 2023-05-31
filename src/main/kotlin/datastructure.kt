233014d46bb611d723d79a5086415501bc7c319a
Ikhsan Assidiqie <ikhsansdqq@gmail.com>
2023-05-31 17:25:12 +0700

    adding datastructure.kt

---
+++ src/main/kotlin/datastructure.kt
@@ -0,0 +1,14 @@
+fun main() {
+//    DATA STRUCTURE
+    val iniArray = mutableListOf(2, 4, 10, 5, 15, 3)
+//    while (iniArray.isNotEmpty()) {
+//        val removingElement = iniArray.removeAt(0)
+//        println("Removed element: $removingElement")
+//    }
+
+//    MAXIMUM
+    println(iniArray.max())
+    println(iniArray.min())
+
+    println("The array is now empty!")
+}
